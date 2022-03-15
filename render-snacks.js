850d619d4e75e82160aee28135171ee06266e737
chris_chan <chrischandfw@gmail.com>
2022-03-15 14:24:16 -0500

    created dom elements and text content for first array

--- render-snacks.js
+++ render-snacks.js
@@ -1,3 +1,30 @@
 export function renderSnackEl(snack) {
-	
+	/*<div class="snacks">
+	<img src="./assets/skittles.png"/>
+	<h3>Name: Skittles</h3>
+	<p>Wholesale: 3 USD</p>
+	<p>Retail: 7 USD</p>
+	<p>Country of Origin: USA</p>
+</div>*/
+    const snackEl = document.createElement('div');
+    const imgEl = document.createElement('img');
+    const nameEl = document.createElement('h3');
+    const wholesaleEl = document.createElement('p');
+    const retailEl = document.createElement('p');
+    const countryEl = document.createElement('p');
+
+    snackEl.classList.add('snack');
+    snackEl.style.background = snack.color;
+
+    imgEl.src = `assets/${snack.name}.png`;
+    nameEl.textContent = (snack.name);
+    wholesaleEl.textContent = `Wholesale: ${snack.cost}`;
+    retailEl.textContent = `Retail: ${snack.retailPrice}`;
+    countryEl.textContent = snack.country;
+
+    snackEl.append(imgEl, nameEl, wholesaleEl, retailEl, countryEl);
+
+    return snackEl;
+
+
 }
