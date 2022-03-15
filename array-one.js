bd585735129976881fbe7f1af9086bcea13f53b6
chris_chan <chrischandfw@gmail.com>
2022-03-15 13:18:33 -0500

    added first aray

---
+++ array-one.js
@@ -0,0 +1,23 @@
+export const snacks = [
+    {
+        name: 'Skittles',
+        cost: '3',
+        retailPrice: '7',
+        country: 'USA'
+    },
+    {
+        name: 'Hi-Chew',
+        cost: '5',
+        retailPrice: '11',
+        country: 'Japan'
+    },
+    {
+        name: 'Tao Kae Noi',
+        cost: '4',
+        retailPrice: '9',
+        country: 'Thailand'
+    },
+    {
+        name: '' 
+    }
+];
